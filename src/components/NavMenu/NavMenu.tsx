2e38a9eb7fb0951bb3975f4edf1d2818b076277a
Ivan Efimov <efimov_il@sch2001.ru>
2021-12-22 13:19:06 +0300

    Мелкие правки по коду.

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -15,13 +15,15 @@ function navLinkClass(params: NavLinkClassParams): string {
 const NavMenu: FC = () => {
   return (
     <nav className="nav-menu">
-      {routes.map((route) => (
-        <div key={route.path}>
-          <NavLink className={navLinkClass} to={route.path}>
-            {route.title}
-          </NavLink>
-        </div>
-      ))}
+      <ul className="nav-menu-items">
+        {routes.map((route) => (
+          <li key={route.path}>
+            <NavLink className={navLinkClass} to={route.path}>
+              {route.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
