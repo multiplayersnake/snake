2fa67037b548e90dc73bd485f1b38fa6569e6ada
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-12-23 08:13:37 +0700

    snake-08 add ErrorBoundary

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import NavMenu from '../NavMenu';
+import ErrorBoundary from '../ErrorBoundary';
 
 import LoginPage from '../../pages/LoginPage';
 import SignupPage from '../../pages/SignupPage';
@@ -9,18 +10,29 @@ import LeaderboardPage from '../../pages/LeaderboardPage';
 import ForumPage from '../../pages/ForumPage';
 import GamePage from '../../pages/GamePage';
 
+import './App.css';
+
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <NavMenu hidden={false} />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
-        <Route path="/forum" element={<ForumPage />} />
-        <Route path="/game" element={<GamePage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <NavMenu hidden={false} />
+        <main className="content">
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/leaderboard" element={<LeaderboardPage />} />
+              <Route path="/forum" element={<ForumPage />} />
+              <Route path="/game" element={<GamePage />} />
+
+              <Route path="/" element={<Navigate to="/login" />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </ErrorBoundary>
+        </main>
+      </BrowserRouter>
+    </div>
   );
 };
 
---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logReactRenderError from './logReactRenderError';
+
+import './ErrorBoundary.css';
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends React.Component<unknown, ErrorBoundaryState> {
+  public state = { hasError: false };
+
+  private static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    logReactRenderError(error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-message-container">
+          <span className="error-message">Произошла неустранимая ошибка, попробуйте обновить приложение</span>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
