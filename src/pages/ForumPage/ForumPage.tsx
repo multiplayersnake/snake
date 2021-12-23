e8c748a497dadf52440830418742e8b85c730a32
Ivan Efimov <efimov_il@sch2001.ru>
2021-12-23 12:34:40 +0300

    Начало разработки страницы форума.

--- src/pages/ForumPage/ForumPage.tsx
+++ src/pages/ForumPage/ForumPage.tsx
@@ -3,13 +3,15 @@ import React, { FC } from 'react';
 import Button from '../../components/Button';
 
 import './ForumPage.css';
+import cn from 'classnames';
 
 const ForumPage: FC = () => {
   return (
     <div className="forum-page">
-      <h1>Здесь будет форум...</h1>
-      <h2>И мы пробуем начать разработку этой страницы</h2>
-      <Button>OK</Button>
+      <Button className={cn('button-forum-back')}>В меню</Button>
+      <div className={cn('title', 'title-forum')}>Форум</div>
+      <div className={cn('title', 'themes-forum')}>Форум</div>
+      <div className={cn('title', 'new-forum')}>Форум</div>
     </div>
   );
 };
