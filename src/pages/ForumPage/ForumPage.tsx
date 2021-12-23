67e35828a8111c538c65465b843842513604ae50
Ivan Efimov <efimov_il@sch2001.ru>
2021-12-23 21:39:54 +0300

    Улучшил разметку, добавил разметку для создания новой темы.

--- src/pages/ForumPage/ForumPage.tsx
+++ src/pages/ForumPage/ForumPage.tsx
@@ -6,39 +6,12 @@ import Theme from '../../components/Theme';
 
 import './ForumPage.css';
 import cn from 'classnames';
+import { out_arr } from './mock';
 
 const ForumPage: FC = () => {
-  // Временный массив - заглушка. Эти данные должны приходить из API запроса.
-  const out_arr = [];
-  out_arr.push({
-    id: 1,
-    datetime: '01.09.2021',
-    author: 'John1',
-    mesCount: 15,
-    newCount: 2,
-    content: 'Чем управлять?'
-  });
-  out_arr.push({
-    id: 2,
-    datetime: '01.10.2021',
-    author: 'John2',
-    mesCount: 16,
-    newCount: 0,
-    content: 'Пожелания к разработчикам'
-  });
-  out_arr.push({
-    id: 3,
-    datetime: '01.11.2021',
-    author: 'John3',
-    mesCount: 17,
-    newCount: 3,
-    content: 'Какая скучная игра!'
-  });
-  out_arr.push({ id: 4, datetime: '01.12.2021', author: 'John4', mesCount: 18, newCount: 4, content: 'Конкурс!' });
-
   return (
     <div className="forum-page">
-      <Button className={cn('button-forum-back')}>В меню</Button>
+      <Button className={cn('button', 'button-forum-back')}>В меню</Button>
       <Heading tag="h1" className={cn('title-forum')}>
         Форум
       </Heading>
@@ -54,7 +27,13 @@ const ForumPage: FC = () => {
           />
         ))}
       </div>
-      <div className={cn('title', 'new-forum')}>Форум</div>
+      <div className={cn('new-forum')}>
+        Новая тема:
+        <input className={cn('input')} />
+        Сообщение:
+        <textarea className={cn('text-area', 'text-area-forum')} />
+        <button className={cn('button', 'button-forum-new')}>Создать тему</button>
+      </div>
     </div>
   );
 };
