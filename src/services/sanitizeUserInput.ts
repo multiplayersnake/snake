79e8204074afc79d55b16a4238625987186fb993
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-12-23 12:32:57 +0700

    snake-17 fix sanitizeUserInput

--- src/services/sanitizeUserInput.ts
+++ src/services/sanitizeUserInput.ts
@@ -1,13 +1,10 @@
-// import sanitizeHtml from 'sanitize-html';
+import sanitizeHtml from 'sanitize-html';
 
-// TODO rework
-const sanitizeUserInput = (userInput: string): string => userInput;
-
-// const sanitizeUserInput = (userInput: string): string =>
-//   sanitizeHtml(userInput, {
-//     allowedTags: [],
-//     allowedAttributes: {},
-//     disallowedTagsMode: 'discard'
-//   });
+const sanitizeUserInput = (userInput: string): string =>
+  sanitizeHtml(userInput, {
+    allowedTags: [],
+    allowedAttributes: {},
+    disallowedTagsMode: 'discard'
+  });
 
 export default sanitizeUserInput;
