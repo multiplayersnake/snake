5e784a0f6e8802217997b31e87fb846905418cbd
Ivan Efimov <efimov_il@sch2001.ru>
2021-12-24 10:48:10 +0300

    Создал функциональность (пока на заглушках) создания новой темы.

--- src/pages/ForumPage/ForumPage.tsx
+++ src/pages/ForumPage/ForumPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useState } from 'react';
 
 import Button from '../../components/Button';
 import Heading from '../../components/Heading';
@@ -8,7 +8,39 @@ import './ForumPage.css';
 import cn from 'classnames';
 import { out_arr } from './mock';
 
+function formatDateTime(val: Date) {
+  const tmp = [
+    '0' + val.getDate(),
+    '0' + (val.getMonth() + 1),
+    '' + val.getFullYear(),
+    '0' + val.getHours(),
+    '0' + val.getMinutes()
+  ].map((component) => component.slice(-2)); // взять последние 2 цифры из каждой компоненты
+  return tmp.slice(0, 3).join('.') + ' ' + tmp.slice(3).join(':');
+}
+
 const ForumPage: FC = () => {
+  const contentRef = useRef(null);
+  const [themes, setThemes] = useState(out_arr);
+
+  const CreateNewTheme = () => {
+    const id = themes.length;
+    const datetime = formatDateTime(new Date());
+    const content = contentRef.current.value;
+    setThemes(
+      [
+        {
+          id: id,
+          datetime: datetime,
+          author: 'Текущий пользователь',
+          mesCount: 1,
+          newCount: 0,
+          content: content
+        }
+      ].concat(themes)
+    );
+  };
+
   return (
     <div className="forum-page">
       <Button className={cn('button', 'button-forum-back')}>В меню</Button>
@@ -16,7 +48,7 @@ const ForumPage: FC = () => {
         Форум
       </Heading>
       <div className={cn('themes-forum')}>
-        {out_arr.map((value, index) => (
+        {themes.map((value, index) => (
           <Theme
             key={index}
             datetime={value.datetime}
@@ -29,10 +61,12 @@ const ForumPage: FC = () => {
       </div>
       <div className={cn('new-forum')}>
         Новая тема:
-        <input className={cn('input')} />
+        <input ref={contentRef} className={cn('input')} />
         Сообщение:
         <textarea className={cn('text-area', 'text-area-forum')} />
-        <button className={cn('button', 'button-forum-new')}>Создать тему</button>
+        <button onClick={CreateNewTheme} className={cn('button', 'button-forum-new')}>
+          Создать тему
+        </button>
       </div>
     </div>
   );
