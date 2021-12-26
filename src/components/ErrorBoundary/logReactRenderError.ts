d00f7fb0136b9f644118c75d28ee3da5c20ea124
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-12-26 09:29:47 +0700

    snake-07 fix logReactRenderError

--- src/components/ErrorBoundary/logReactRenderError.ts
+++ src/components/ErrorBoundary/logReactRenderError.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function logReactRenderError(error: Error, errorInfo: React.ErrorInfo) {
-  console.log('React render error:', error, errorInfo);
+  console.error('React render error:', error, errorInfo);
 }
 
 export default logReactRenderError;
