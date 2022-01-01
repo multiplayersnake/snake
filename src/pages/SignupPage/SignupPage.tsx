125ac49749269ebd15c56a071b264c1c9c885a05
Lvov Aleksandr <prince71188@gmail.com>
2022-01-02 01:55:55 +0300

    merged from several branches

--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -1,15 +1,25 @@
 import React, { FC } from 'react';
 
+import logo from '../../assets/logo.png';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import './SignupPage.css';
+import Heading from '../../components/Heading';
+import Form from '../../components/Form';
 
 const SignupPage: FC = () => {
   return (
     <div className="signup-page">
-      <h1>Здесь будет страница регистрации...</h1>
-      <Button>Зарегистрироваться</Button>
+      <img className="logo" src={logo} alt={'logo'} />
+      <Form>
+        <Heading tag="h1">Регистрация</Heading>
+        <Input label="Логин" name="login" />
+        <Input label="Пароль" name="password" type="password" />
+        <div className="login-page-buttons">
+          <Button type="submit">Зарегистрироваться</Button>
+        </div>
+      </Form>
     </div>
   );
 };
