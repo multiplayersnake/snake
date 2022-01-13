6eda3b37195406532f4c8916f446ac20d6de3b20
Ivan Efimov <efimov_il@sch2001.ru>
2022-01-13 11:29:32 +0300

    Добавил панели информации и таблицы лидеров на главную.

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from 'react';
+import cn from 'classnames';
 
 import Heading from '../../components/Heading';
 import MainMenuItem from '../../components/MainMenuItem';
 import { MenuAction } from '../../types/mainMenu';
+import coinSource from '../../assets/coin.png';
+import awardSource from '../../assets/award.png';
 
 import menuItems from './menuItems';
 
 import './MainPage.css';
+import { leaderList } from './mock';
 
 type MainPageProps = {
   authorized: boolean;
@@ -22,6 +26,31 @@ const MainPage: FC<MainPageProps> = ({ onAction, authorized }) => {
       <div className="main-page-menu">
         {menuItems.map((item) => (
           <MainMenuItem key={item.title} item={item} onAction={onAction} authorized={authorized} />
+        ))}
+      </div>
+      <div className={cn('panel', 'left-panel')}>
+        <div className={cn('heading', 'h6')}>Позывной:</div>
+        <div className={cn('heading', 'h6')}>John</div>
+        <div className={'flex-wrapper'} />
+        <div>Валюта:</div>
+        <div>
+          150 <img src={coinSource} className={cn('img-in-line')} alt={'coin'} />
+        </div>
+        <div className={'flex-wrapper'} />
+        <div>Награды: </div>
+        <div>
+          7 <img src={awardSource} className={cn('img-in-line')} alt={'award'} />
+        </div>
+      </div>
+      <div className={cn('panel', 'right-panel')}>
+        <div className={cn('heading', 'h6')}>Лучшие из лучших:</div>
+        {leaderList.map((item) => (
+          <div key={item.nick} className={'leader-item'}>
+            <div> {item.nick} </div>
+            <div>
+              {item.awards} <img src={awardSource} className={cn('img-in-line')} alt={'award'} />
+            </div>
+          </div>
         ))}
       </div>
     </div>
---
+++ src/pages/MainPage/mock.ts
@@ -0,0 +1,12 @@
+export const leaderList = [
+  { nick: 'John1', awards: 133 },
+  { nick: 'John2', awards: 126 },
+  { nick: 'John3', awards: 83 },
+  { nick: 'John4', awards: 71 },
+  { nick: 'John5', awards: 69 },
+  { nick: 'John6', awards: 54 },
+  { nick: 'John7', awards: 40 },
+  { nick: 'John8', awards: 32 },
+  { nick: 'John9', awards: 21 },
+  { nick: 'John10', awards: 14 }
+];
