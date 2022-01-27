e1bc2f0dfca2d9cf8de89e49ce5e546c1b516013
Ivan Efimov <efimov_il@sch2001.ru>
2022-01-27 14:29:17 +0300

    Внедрил PlayWright для тестирования пользовательских сценариев.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve(
       './src/__test__/__mock__/styleMock.js'
