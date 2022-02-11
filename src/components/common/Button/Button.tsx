5afe38251db99298f538ba14463c00f2a16562cf
Ivan Efimov <efimov_il@sch2001.ru>
2022-02-11 14:08:00 +0300

    Добавил звук на обычную кнопку тоже.

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,15 +1,31 @@
-import React, { FC, ButtonHTMLAttributes } from 'react';
+import React, { FC, ButtonHTMLAttributes, useRef, useCallback } from 'react';
 import cn from 'classnames';
 
 import './Button.css';
+import mscHover from '../../../assets/sound/button_hover.mp3';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button: FC<ButtonProps> = (props) => {
   const { type = 'button', children, className, ...rest } = props;
 
+  const ref = useRef(null);
+  const hover = useCallback(() => {
+    const obj = ref.current as HTMLAudioElement;
+    obj.volume = 0.2;
+    obj
+      .play()
+      .then(function () {
+        // console.log('playing');
+      })
+      .catch(function (reason) {
+        console.log(reason);
+      });
+  }, []);
+
   return (
-    <button {...rest} className={cn('button', className)} type={type}>
+    <button {...rest} className={cn('button', className)} type={type} onMouseEnter={hover}>
+      <audio ref={ref} src={mscHover} autoPlay={false} preload={'auto'} />
       {children}
     </button>
   );
