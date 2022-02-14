b10f82f270feff3f06a4e0b840070284f78e0966
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2022-02-14 15:35:05 +0700

    SNAKE-43 add come TODOs

--- src/ssrStore/rootStore.ts
+++ src/ssrStore/rootStore.ts
@@ -1,9 +1,11 @@
 import {
   createStore
+  // TODO проверить, работает ли стор без routerMiddleware
   // applyMiddleware,
   // compose
 } from 'redux';
 import { createBrowserHistory, createMemoryHistory } from 'history';
+// TODO проверить, работает ли стор без routerMiddleware
 // import { routerMiddleware } from 'connected-react-router'
 import createRootReducer from './rootReducer';
 import { SsrState } from './types';
@@ -16,7 +18,7 @@ export function configureStore(initialState: SsrState, url = '/') {
   const store = createStore(
     createRootReducer(history),
     initialState
-    // TODO это нужно или нет ?
+    // TODO проверить, работает ли стор без routerMiddleware
     // compose(
     //     applyMiddleware(
     //         routerMiddleware(history)
