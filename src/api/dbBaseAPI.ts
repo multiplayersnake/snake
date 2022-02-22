a75dda3902d716f56478bc65ea6402f43482a57d
Ivan Efimov <efimov_il@sch2001.ru>
2022-02-22 11:16:15 +0300

    Убрал дублирующие файлы.

--- src/api/dbBaseAPI.ts
+++
@@ -1,9 +0,0 @@
-import HTTP from './dbHTTP';
-
-export default abstract class BaseAPI {
-  protected http: HTTP;
-
-  protected constructor(endpoint: string) {
-    this.http = new HTTP(endpoint);
-  }
-}
