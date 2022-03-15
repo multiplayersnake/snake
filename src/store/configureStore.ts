2d75c5be27b080ccd607c14e7b099c9305a28ae8
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2022-03-15 12:38:58 +0700

    SNAKE-58 configureStore: use thunk

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,12 +1,6 @@
-import {
-  createStore,
-  // TODO проверить, работает ли стор без routerMiddleware
-  applyMiddleware,
-  compose
-} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { createBrowserHistory, createMemoryHistory } from 'history';
-// TODO проверить, работает ли стор без routerMiddleware
-import { routerMiddleware } from 'connected-react-router';
 
 import { isServer } from '../utils';
 import { Indexed } from '../types';
@@ -15,12 +9,7 @@ import { createRootReducer } from './reducers';
 
 export function configureStore(initialState: Indexed = {}, url = '/') {
   const history = isServer ? createMemoryHistory({ initialEntries: [url] }) : createBrowserHistory();
-  const store = createStore(
-    createRootReducer(history),
-    initialState,
-    // TODO проверить, работает ли стор без routerMiddleware
-    compose(applyMiddleware(routerMiddleware(history)))
-  );
+  const store = createStore(createRootReducer(history), initialState, compose(applyMiddleware(thunk)));
 
   return { store, history };
 }
