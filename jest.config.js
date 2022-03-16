072eb84acaddd949cf5869638d8bf2586cb49e2b
Ivan Efimov <efimov_il@sch2001.ru>
2022-03-16 14:55:00 +0300

    Восстановление работы jest.

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/tests/'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|fbx)$': require.resolve(
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve(
       './src/__test__/__mock__/styleMock.js'
     ),
-    '\\.(css|less)$': require.resolve('./src/__test__/__mock__/styleMock.js'),
-    '\\FBXLoader$': require.resolve('./src/__test__/__mock__/styleMock.js')
+    '\\.(css|less)$': require.resolve('./src/__test__/__mock__/styleMock.js')
   }
 };
